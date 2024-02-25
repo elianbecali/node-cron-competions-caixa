b0c41cf35531628e8c852c573b0b1e7512219e15
Elian Becali <elianbecaliaguiar@gmail.com>
2024-02-24 21:15:36 -0400

    chore: setup server with express

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 require('dotenv').config()
+import express from 'express'
 import cron from 'node-cron';
 import { cronRunLotofacilResults } from './crons/lotofacil';
 import { cronRunMegasenaResults } from './crons/megasena';
 
+const app = express()
+
 // Segunda á sábado
-cron.schedule('00 22 * * 1-6', cronRunLotofacilResults, { timezone: 'America/Sao_Paulo' })
+cron.schedule('20 22 * * 1-6', cronRunLotofacilResults, { timezone: 'America/Sao_Paulo' })
 
 // Terça, Quinta e Sábado
-cron.schedule('00 22 * * 2,4,6', cronRunMegasenaResults, { timezone: 'America/Sao_Paulo' })
+cron.schedule('20 22 * * 2,4,6', cronRunMegasenaResults, { timezone: 'America/Sao_Paulo' })
 
+app.listen(3333, () => {
+  console.log('Server Running on port 3333')
+})
