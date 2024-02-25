822dcc60ad10a9f4329ffd999f96a244751ebdbe
Elian Becali <elianbecaliaguiar@gmail.com>
2024-02-24 21:20:39 -0400

    chore: config setup PORT env

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { cronRunLotofacilResults } from './crons/lotofacil';
 import { cronRunMegasenaResults } from './crons/megasena';
 
 const app = express()
+const SERVER_PORT = process.env.PORT ?? 3000
 
 // Segunda á sábado
 cron.schedule('20 22 * * 1-6', cronRunLotofacilResults, { timezone: 'America/Sao_Paulo' })
@@ -12,6 +13,6 @@ cron.schedule('20 22 * * 1-6', cronRunLotofacilResults, { timezone: 'America/Sao
 // Terça, Quinta e Sábado
 cron.schedule('20 22 * * 2,4,6', cronRunMegasenaResults, { timezone: 'America/Sao_Paulo' })
 
-app.listen(3333, () => {
+app.listen(SERVER_PORT, () => {
   console.log('Server Running on port 3333')
 })
