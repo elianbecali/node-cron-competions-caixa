7ca58475dd6646f64eccf31af85d39e5e1a95b3e
Elian Becali <elianbecaliaguiar@gmail.com>
2024-06-24 16:08:38 -0400

    fix: token api loteria expires at 24-06-2025

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,7 @@ const BASE_URL_FEZINHA = "https://shark-app-5k78i.ondigitalocean.app"
 export const apiLoteria = axios.create({
   baseURL: "https://apiloterias.com.br/app/v2",
   params: {
-    token: 'OQue7Ww85yYH5cu'
+    token: 'fkCRUwH0AysZ3Jn'
   }
 })
 export const apiFezinhaOnline = axios.create({
